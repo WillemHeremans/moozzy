7c371744810653f27af78ea89574b3600ae0bb16
WillemHeremans <willemheremans@gmail.com>
2019-03-04 00:17:57 +0100

    Music Player

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 
-let play  = true;
+
+
+let play = true;
 let pause = false;
-let music = new Audio('Blue_Dot_Sessions.mp3');
+let music = new Audio('http://broadcast.infomaniak.ch/start-adofm-high.mp3');
 music.preload = 'metadata';
 // let start = 0;
 // let duration = 0;
 let progressBar = document.getElementById('progressBar');
-let playPause =  document.getElementById('playPause');
+let playPause = document.getElementById('playPause');
 let durationMetaData = document.getElementById('durationMetaData');
 progressBar.setAttribute('value', music.currentTime.toString())
 progressBar.setAttribute('max', music.duration.toString())
 console.log(music);
 
-music.onloadedmetadata = function() {
+music.onloadedmetadata = function () {
   duration = music.duration;
   start = music.currentTime;
   durationMetaData.innerHTML = start + ' / ' + duration;
 }
-
 
 function playButton() {
   if (play) {
@@ -57,10 +58,53 @@ function progressClick(event) {
   let currentValue = (event.clientX) - (event.target['offsetLeft']).toString();
   event.target['max'] = (maxValue / maxValue) * duration;
   event.target['value'] = (currentValue / maxValue) * duration;
-  start = event.target['value'];
-  console.log(event);
-  console.log(currentValue + ' / ' + maxValue);
-  console.log( ((currentValue / maxValue) * duration) + ' / ' + ((maxValue / maxValue) * duration) );
-  console.log(start);
+  if (!play) {
+    music.pause();
+    play = true;
+    start = event.target['value'];
+    playButton();
+  } else {
+    start = event.target['value'];
+    playButton();
+  }
+}
+
+function addFile() {
+  let inp = document.getElementById("get-files");
+// Access and handle the files 
+for (i = 0; i < inp.files.length; i++) {
+  let file = inp.files[i];
+  // do things with file
+  console.log(file['name']);
+  openIndexedDB(file['name']);
+}
 
 }
+
+
+function openIndexedDB (fileindex) {
+  // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+  var indexedDB = window.indexedDB;
+
+  var openDB = indexedDB.open("MyDatabase", 1);
+
+  openDB.onupgradeneeded = function() {
+    var db = {}
+    db.result = openDB.result;
+    db.store = db.result.createObjectStore("MyObjectStore", {keyPath: "id"});
+    if (fileindex) db.index = db.store.createIndex("NameIndex", fileindex);
+  };
+
+  console.log(openDB);
+}
+
+function getStoreIndexedDB (openDB) {
+  var db = {};
+  db.result = openDB.result;
+  db.tx = db.result.transaction("MyObjectStore", "readwrite");
+  db.store = db.tx.objectStore("MyObjectStore");
+  db.index = db.store.index("NameIndex");
+
+  console.log(db);
+}
+
