39325dbab0bff3893918256725f395d891a9c74b
WillemHeremans <willemheremans@gmail.com>
2019-03-12 15:55:46 +0100

    Music Player

--- js/database.js
+++ js/database.js
@@ -1,3 +1,54 @@
+let indexedDB = window.indexedDB
+let dbVersion = 4;
+
+function loadData() {
+
+  let request = indexedDB.open("songs", dbVersion);
+
+    createObjectStore = function (dataBase) {
+      // Create an objectStore
+      console.log("Creating objectStore")
+      dataBase.createObjectStore("MySongs");
+    }
+
+  request.onerror = function (event) {
+    console.log("Error creating/accessing IndexedDB database");
+  };
+
+  request.onsuccess = function (event) {
+    console.log("Success creating/accessing IndexedDB database :");
+    db = request.result;
+
+    let transaction = db.transaction(["MySongs"], "readonly");
+    let getDB = transaction.objectStore("MySongs").getAll();
+    let countData = transaction.objectStore("MySongs").count();
+    // let monIndex = transaction.objectStore("MySongs").createIndex('NomIndex', 'Maclé', { unique: false, locale: 'fr-FR' });
+
+    getDB.onsuccess = function () {
+      let tracks = getDB.result;
+      let displayData = document.getElementById('datas');
+      for (i in tracks) {
+        console.log(tracks[i]);
+        let para = document.createElement('P');
+        let songData = document.createTextNode(tracks[i]);
+        para.appendChild(songData);
+        displayData.appendChild(para);
+      }
+    }
+
+    countData.onsuccess = function() {
+      console.log(countData.result);
+    }
+
+    db.onerror = function (event) {
+      console.log("Error creating/accessing IndexedDB database");
+    };
+
+  }
+}
+
+
+
 function addFile() {
 
   let inp = document.getElementById("get-files");
@@ -6,48 +57,47 @@ function addFile() {
     let file = inp.files[i];
     // do things with file
     console.log(file['name']);
-  console.log('Ce script est bien appelé')
-  // IndexedDB
-  var indexedDB = window.indexedDB
-      dbVersion = 1.0;
+    console.log('Ce script est bien appelé')
+    // IndexedDB
 
-  // Create/open database
-  var request = indexedDB.open("songs", dbVersion),
+
+    // Create/open database
+    var request = indexedDB.open("songs", dbVersion),
       db,
       createObjectStore = function (dataBase) {
-          // Create an objectStore
-          console.log("Creating objectStore")
-          dataBase.createObjectStore("MySongs");
+        // Create an objectStore
+        console.log("Creating objectStore")
+        dataBase.createObjectStore("MyNewData");
       }
 
-  request.onerror = function (event) {
+    request.onerror = function (event) {
       console.log("Error creating/accessing IndexedDB database");
-  };
+    };
 
-  request.onsuccess = function (event) {
+    request.onsuccess = function (event) {
       console.log("Success creating/accessing IndexedDB database");
       db = request.result;
-    console.log(db);
+      console.log(db);
 
 
-    var transaction = db.transaction(["MySongs"], "readwrite");
-    console.log(transaction.objectStore("MySongs").put(file['name'], new Date().toLocaleString('fr-FR')));
-    let getDB = transaction.objectStore("MySongs").getAll();
-    let getKey = transaction.objectStore("MySongs").get('juno');
-  
-    getDB.onsuccess = function () {
-      let tracks = getDB.result;
-      // var table = document.getElementById("tBody");
-      for (i in tracks) {
-        console.log(tracks[i]);
-  //       var row = table.insertRow(i);
-  //       var cell1 = row.insertCell(i);
-  // cell1.innerHTML = 'tracks[i]';
-      }
+      var transaction = db.transaction(["MySongs"], "readwrite");
+      console.log(transaction.objectStore("MySongs").put(file['name'], new Date().toLocaleString('fr-FR')));
+      let getDB = transaction.objectStore("MySongs").getAll();
+      let getKey = transaction.objectStore("MySongs").get('juno');
 
-      getKey.onsuccess = function() {
-        let trackTime = getKey.result;
-        // for (i in trackTime) {
+      getDB.onsuccess = function () {
+        let tracks = getDB.result;
+        // var table = document.getElementById("tBody");
+        for (i in tracks) {
+          console.log(tracks[i]);
+          //       var row = table.insertRow(i);
+          //       var cell1 = row.insertCell(i);
+          // cell1.innerHTML = 'tracks[i]';
+        }
+
+        getKey.onsuccess = function () {
+          let trackTime = getKey.result;
+          // for (i in trackTime) {
           console.log('result de get : ');
           console.log(trackTime);
           document.getElementById('music').src = trackTime;
@@ -59,20 +109,19 @@ function addFile() {
             durationMetaData.innerHTML = start + ' / ' + duration;
             console.log(music.mozGetMetadata());
           }
-        // }
+          // }
+        }
 
       }
-
-    }
       db.onerror = function (event) {
-          console.log("Error creating/accessing IndexedDB database");
+        console.log("Error creating/accessing IndexedDB database");
       };
-      
-  }
-  
-  // For future use. Currently only in latest Firefox versions
-  request.onupgradeneeded = function (event) {
+    }
+
+    // For future use. Currently only in latest Firefox versions
+    request.onupgradeneeded = function (event) {
       createObjectStore(event.target.result);
       console.log(event.target.result);
-  };
-}}
+    };
+  }
+}
